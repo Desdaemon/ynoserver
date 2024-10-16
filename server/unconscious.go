95aa426cac8ab40ac6e0b1c6cdfffe651801bf7f
Viet Dinh <54ckb0y789@gmail.com>
2024-10-15 20:28:27 -0400

    Add more syncvars for Collective Unconscious

--- server/unconscious.go
+++ server/unconscious.go
@@ -1,8 +1,13 @@
 package server
 
-import "time"
+import (
+	"math"
+	"math/rand/v2"
+	"time"
+)
 
 var startTime = time.Now()
+var temperature, precipitation int
 
 func initUnconscious() {
 	scheduler.Every(1).Minute().Do(func() {
@@ -18,8 +23,40 @@ func initUnconscious() {
 			}
 		}
 	})
+	scheduler.Every(2).Minutes().Do(func() {
+		temperature = max(-100, min(100, temperature+weatherDelta(temperature)))
+		precipitation = max(0, min(100, precipitation+weatherDelta(precipitation)))
+		for _, client := range clients.Get() {
+			if client.roomC == nil {
+				continue
+			}
+
+			select {
+			case client.roomC.outbox <- buildMsg("cuw", temperature, precipitation):
+			default:
+				writeErrLog(client.uuid, client.roomC.mapId, "send channel is full")
+			}
+		}
+	})
+}
+
+func weatherDelta(n int) int {
+	var sign float64 = 1
+	if n < 0 {
+		sign = -1
+	}
+	return int(rand.Int32N(21)) - 10 + int(math.Round(math.Pow(float64(n)/100, 2))*sign*-4)
 }
 
 func getUnconsciousTime() int {
 	return int(time.Since(startTime).Minutes()) % 240
+}
+
+func didJoinRoomUnconscious(c *RoomClient) {
+	if c == nil {
+		return
+	}
+
+	c.outbox <- buildMsg("cut", getUnconsciousTime())
+	c.outbox <- buildMsg("cuw", temperature, precipitation)
 }
